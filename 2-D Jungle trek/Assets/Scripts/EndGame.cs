e652989ccfe599b3ec34ba246c64d64d9201f0cc
Eric Olson <Eric.Olson3@iam.local>
2018-12-12 13:07:50 -0600

    finished start screen, made game loop

---
+++ 2-D Jungle trek/Assets/Scripts/EndGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    private bool isPlayerInTrigger;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInTrigger = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInTrigger = false;
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetButtonDown("Activate") && isPlayerInTrigger)
+        {
+            Debug.Log("Player activated door!");
+            SceneManager.LoadScene("Level0");
+        }
+    }
+
+}
